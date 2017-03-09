019df3172422b7d8fb36ad5ab3588ca048e40e6a
Mario Marroquin <mario@carbonfive.com>
2017-03-09 15:44:37 -0800

    Created models

---
+++ app/routes/eventHandler.js
@@ -0,0 +1,56 @@
+/*
+ * All callbacks for Messenger are POST-ed. They will be sent to the same
+ * webhook. Be sure to subscribe your app to your page to receive callbacks
+ * for your page. 
+ * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
+ *
+ */
+
+var setUp = require('../models/setUp.js') 
+var getStarted = require('../models/getStarted')
+var app = setUp.app
+PAGE_ACCESS_TOKEN = setUp.PAGE_ACCESS_TOKEN
+var receivedPostback = getStarted.receivedPostback
+var sendTextMessage = getStarted.sendTextMessage
+var callSendAPI = getStarted.callSendAPI
+  request = require('request');
+
+module.exports = function (req, res) {
+  var data = req.body;
+
+ // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          /* receivedMessage(messagingEvent);*/
+        } else if (messagingEvent.delivery) {
+          receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          receivedPostback(messagingEvent);
+        } else if (messagingEvent.read) {
+          receivedMessageRead(messagingEvent);
+        } else if (messagingEvent.account_linking) {
+          receivedAccountLink(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've 
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
+  }
+}
+
