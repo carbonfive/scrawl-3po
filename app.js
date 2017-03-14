03013fba9fbe0609e270ccde8a372437724db3bf
Mario Marroquin <mario@carbonfive.com>
2017-03-14 13:41:10 -0700

    Update verification of request into its own module

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var verifyRequestSignature = require('./app/models/verify')
+
 const 
   bodyParser = require('body-parser'),
   crypto = require('crypto'),
@@ -15,27 +17,5 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
 app.use('/', router);
-
-function verifyRequestSignature(req, res, buf) {
-  var signature = req.headers["x-hub-signature"];
-
-  if (!signature) {
-    // For testing, let's log an error. In production, you should throw an 
-    // error.
-    console.error("Couldn't validate the signature.");
-  } else {
-    var elements = signature.split('=');
-    var method = elements[0];
-    var signatureHash = elements[1];
-
-    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
-                        .update(buf)
-                        .digest('hex');
-
-    if (signatureHash != expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
-    }
-  }
-}
 
 module.exports = app;
