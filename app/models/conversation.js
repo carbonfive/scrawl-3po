d356a04b1acb4549f8cbf9930de7371030e15c51
Mario Marroquin <mario@carbonfive.com>
2017-03-23 13:35:50 -0700

    Update conversation model to be cleaner

--- app/models/conversation.js
+++ app/models/conversation.js
@@ -1,41 +1,33 @@
 var users = {};
+var convo = {'introduction': ["Greetings, I'm Scrawl-3PO, assembled by Carbon Five.", "We're going to make an EPIC SPACE OPERA scrawl that you can share with friends."], 
+             'start': ["Let's get started creating your scrawl.", "localhost:3000"] , 
+             'title': ["First, what's the name of your new EPIC SPACE OPERA episode?"],
+             'episode':[]}
 
-
-module.exports.findState = function findState(userID) {
-  return users[userID].state;
-};
-
-module.exports.changeState = function changeState(userID, newState) {
-  console.log(newState);
+function changeState(userID, newState) {
   users[userID] = {state: newState};
 };
 
-module.exports.lookUpMessage = function lookUpMessage(state){
-  var convo = []
-  if (state === "#introduction"){
-  } else if (state === "#start"){
-    convo = ["Lets's get started creating your scrawl"]  
-  } else if (state === "#title"){
-    convo = ["First, what's the name of your new EPIC SPACE OPERA episode?"]
+function findNextState(userID){
+  var state = findState(userID);
+  switch(state) {
+    case undefined:
+      return "start"
+    case "start":
+      return "title";
+    case "title":
+      return "episode";
   }
-  return convo
-};
-
-module.exports.saveData = function saveData(userID, state){
-
 }
 
-module.exports.findNextState = function findNextState(userID){
+function findState(userID) {
   if (users[userID] === undefined){
-    return "#start"
-  }else if(users[userID].state === "#start") {
-    return "#title";
-  } else if (users[userID].state === "title")  {
-    return "#episode";
-  } 
-}
-
-module.exports.text = function text(senderID){
-  var state = findState(senderID)
-  var message = lookUpMessageState(state); 
+    return undefined;
+  }else {
+    return users[userID].state;
+  }
 };
+
+module.exports.convo = convo;
+module.exports.changeState = changeState;
+module.exports.findNextState = findNextState;
