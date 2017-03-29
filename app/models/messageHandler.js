980c123eeb28231c85e97e2b3361f2c262360ab4
Mario Marroquin <mario@carbonfive.com>
2017-03-29 13:42:25 -0700

    Fixed Merging Conflicts

--- app/models/messageHandler.js
+++ app/models/messageHandler.js
@@ -1,11 +1,6 @@
 const reply = require('./reply');
 
-<<<<<<< Updated upstream
-function receivedMessage(event, text){
-  var senderID = event.sender.id;
-=======
 function receivedMessage(senderID, text){
->>>>>>> Stashed changes
   var time = 3000
   text.forEach(function(t){
     setTimeout(function(){ reply.sendTextMessage(senderID, t)}, time);
